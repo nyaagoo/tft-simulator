5ee1ace38a86c335e9c25e5073d138976e48001d
nyaagoo <acc.mg47@gmail.com>
2019-06-19 14:45:22 +0900

    [Initialize] Using mytemplate

---
+++ src/store/index.ts
@@ -0,0 +1,2 @@
+export { counter } from "@/store/modules/counter";
+export { counter1 } from "@/store/modules/counter1";
---
+++ src/views/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .home
+    img(alt="Vue logo" src="../../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js + TypeScript App2")
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/views/Pages/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {}
+</script>
