62b0591592b41405b6be1bb2e61cfbc10df23325
nyaagoo <acc.mg47@gmail.com>
2019-06-19 15:28:36 +0900

    [Update] データjsonファイル追加 データ取得して画面に表示

--- src/views/Pages/Home.vue
+++ src/views/Pages/Home.vue
@@ -1,17 +1,33 @@
 <template lang="pug">
-  .home
-    img(alt="Vue logo" src="../../assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js + TypeScript App2")
+  .home-container
+    .champion-container(v-for="champ in champions" :key="champ.name")
+      p {{ champ.name }}
+      img(height="100" width="100" :src="champ.image")
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/views/Pages/HelloWorld.vue"; // @ is an alias to /src
+import champions from "@/assets/champions.json";
 
 @Component({
   components: {
     HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  champions = champions;
+  greet: string = "hello world";
+}
 </script>
+<style lang="stylus" scoped>
+.home-container
+  margin auto
+  height calc(100vh - 200px)
+  width 600px
+  background #8FF
+  display flex
+  flex-wrap wrap
+.champion-container
+  width 100px
+</style>
