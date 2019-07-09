1634f13eed05130552284a4c7c5f03c9a22c84cd
nyaagoo <acc.mg47@gmail.com>
2019-07-10 00:24:29 +0900

    [Add] font-display swap

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,10 @@
 @import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap&subset=japanese');
 
 #app {
-  font-family: 'Noto Sans JP', sans-serif, 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-family 'Noto Sans JP', sans-serif, 'Avenir', Helvetica, Arial, sans-serif
+  font-display swap
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
 }
 </style>
