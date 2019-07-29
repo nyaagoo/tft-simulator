d23689cb0779ff65d83d12cdc301d037517e5d0a
nyaagoo <acc.mg47@gmail.com>
2019-07-30 00:09:02 +0900

    [Update] アイテムプールのデザイン

--- src/components/Molecules/ItemCombineTable.vue
+++ src/components/Molecules/ItemCombineTable.vue
@@ -60,18 +60,6 @@ export default class CombineTable extends Vue {
   @media screen and (max-width: 600px)
     overflow-x auto
     justify-content flex-start
-.first-row-item-check
-  width 560px
-  margin auto
-  display grid
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  background #424242
-  padding 10px
-  border 1px solid #88ffff
-  box-sizing border-box
-  position relative
-  border-radius 8px
 .first-row-item
   padding 0 0 8px 56px
   display flex
@@ -95,29 +83,6 @@ export default class CombineTable extends Vue {
   @media screen and (max-width: 600px)
     height 36px
     width 36px
-.sub-img
-  height 36px
-  width 36px
-  @media screen and (max-width: 600px)
-    height 24px
-    width 24px
-.sub-img-wrapper
-  height 36px
-  width 36px
-  display inline-block
-  margin-left 8px
-  @media screen and (max-width: 600px)
-    height 24px
-    margin-left 2px
-.hold-item-title
-  font-size 0.8rem
-  position absolute
-  top -10px
-  left 12px
-  background #424242
-  border-top-right-radius 10px
-  border-top-left-radius 10px
-  width 100px
 .conbine-table-description
   display flex
   justify-content center
--- src/components/Organisms/ItemPool.vue
+++ src/components/Organisms/ItemPool.vue
@@ -1,14 +1,10 @@
 <template lang="pug">
   .item-pool-container
-    .basic-item-list.mt-3.mx-0
-      img.item-img.pointer(:src="item.img")(v-for="item in basicItemList" @click="addItemPool(item)")
-    .item-pool
-      img.item-img.pointer(:src="item.img")(v-for="item in itemPool" @click="removeItemPool(item)")
-      // span.hold-item-title 所持アイテム
-      // .hold-item(v-for="item in basicItemList")
-      //   img.main-img.pointer(:class="{grayscale: !isGrayFilterBasicItemColumn(item.id)}" :src="item.img")(@click="ToggleCheckedItemColumn(item.id)")
-      //  span.sub-img-wrapper
-      //    img.sub-img.pointer(v-if="HasSameItem(item.id)" :class="{grayscale: !isGrayFilterBasicItemRow(item.id)}" :src="item.img")(@click="ToggleCheckedItemRow(item.id)")
+    .basic-item-list.pa-3
+      img.item-img.pointer(v-for="item in basicItemList" :src="item.img" @click="addItemPool(item)")
+    .item-pool.pa-3
+      span.item-pool-title アイテムプール
+      img.item-img.pointer(v-for="item in itemPool" :src="item.img" @click="removeItemPool(item)")
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
@@ -38,9 +34,34 @@ export default class ItemPool extends Vue {
 </script>
 <style lang="stylus" scoped>
 .item-pool-container
-  background red
+  width 496px
+  padding 8px
+  overflow-x auto
+  @media screen and (max-width: 600px)
+    width 400px
 .basic-item-list
   display flex
-.item-img + .item-img
-  margin-left  8px
+.item-pool
+  display flex
+  flex-wrap wrap
+  border 1px solid #88f
+  border-radius 8px
+  position relative
+  width 474px
+  @media screen and (max-width: 600px)
+    width 378px
+.item-img
+  height 48px
+  width 48px
+  margin 4px
+  @media screen and (max-width: 600px)
+    height 36px
+    width 36px
+.item-pool-title
+  font-size 0.8rem
+  position absolute
+  top -10px
+  left 12px
+  background #424242
+  width 100px
 </style>
