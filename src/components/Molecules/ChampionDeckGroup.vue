434fcdca78aa553f62e7a55ed02e83e5ce1ee6d3
nyaagoo <acc.mg47@gmail.com>
2019-08-19 17:29:06 +0900

    [Add] ToolTip追加

--- src/components/Molecules/ChampionDeckGroup.vue
+++ src/components/Molecules/ChampionDeckGroup.vue
@@ -2,8 +2,11 @@
   .champion-deck-group.px-3(:style="{'borderColor': borderColor}")
     .class-title
       span {{ groupDescription.name }}
-      span.tooltip-container.synergy-description.mx-1(v-for="(item, index) in groupDescription.synergy" :key="`${groupDescription.id}${index}`") ({{item.require}})
-        span.tooltip {{item.bonus}}}
+      span.tooltip-container.synergy-description.mx-1(v-for="(item, index) in groupDescription.synergy" :key="`${groupDescription.id}${index}`")
+        v-tooltip(top)
+          template(v-slot:activator="{ on }")
+            span(v-on='on') ({{ item.require }})
+          .tooltip-type {{ item.bonus }}
     transition-group.champion-origin-inner(name="flip-list" tag="div")
       .champion-wrapper(v-for="champion in championList" :key="champion.name")
         champion-deck(:champ="champion" :originList="champion.origin" :classList="champion.class" :isDisplayDetail="true")
@@ -28,49 +31,75 @@ export default class ChampionDeckGroup extends Vue {
 }
 </script>
 <style lang="stylus" scoped>
-.champion-deck-group
-  position relative
-  display flex
-  flex-wrap wrap
-  border 1px solid
-  border-radius 8px
-  padding 8px
-.class-title
-  font-size .8rem
-  position absolute
-  top -10px
-  left 12px
-  background #424242
-.champion-container
-  width 100px
-.champion-wrapper
-  p
-    width 60px
-  padding 4px
-.champion-img
-  outline-style solid
-  outline-width 3px
-  outline-offset -3px
-.champion-origin-inner
-  display flex
-  flex-wrap wrap
-.flip-list-item
-  display inline-block
-.flip-list-move
-  transition transform .4s
-.tooltip-container .tooltip
-  visibility hidden
-  width 400px
-  background-color black
-  color #fff
-  text-align center
-  padding 5px 0
-  border-radius 6px
-  position absolute
-  z-index 1
-  bottom 100%
-  left 50%
-  margin-left -200px
-.tooltip-container:hover .tooltip
-  visibility visible
+.champion-deck-group {
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  border: 1px solid;
+  border-radius: 8px;
+  padding: 8px;
+}
+
+.tooltip-type {
+  height: 200px;
+  width: 200px;
+}
+
+.class-title {
+  font-size: 0.8rem;
+  position: absolute;
+  top: -10px;
+  left: 12px;
+  background: #424242;
+}
+
+.champion-container {
+  width: 100px;
+}
+
+.champion-wrapper {
+  p {
+    width: 60px;
+  }
+
+  padding: 4px;
+}
+
+.champion-img {
+  outline-style: solid;
+  outline-width: 3px;
+  outline-offset: -3px;
+}
+
+.champion-origin-inner {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.flip-list-item {
+  display: inline-block;
+}
+
+.flip-list-move {
+  transition: transform 0.4s;
+}
+
+.tooltip-container .tooltip {
+  visibility: hidden;
+  width: 400px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+  position: absolute;
+  z-index: 1;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -200px;
+}
+
+.tooltip-container:hover .tooltip {
+  visibility: visible;
+}
 </style>
